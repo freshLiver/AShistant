07c95eecb9d7ac937f5061120874846a7b6fca50
freshLiver <freshliver.cys@gmail.com>
2021-08-13 03:40:33 +0800

    selected range -> cropped area & can save cropped area to downloads/

--- app/src/main/java/com/freshliver/ashistant/AssistantActivity.java
+++ app/src/main/java/com/freshliver/ashistant/AssistantActivity.java
@@ -2,14 +2,22 @@ package com.freshliver.ashistant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class AssistantActivity extends AppCompatActivity {
 
@@ -17,8 +25,8 @@ public class AssistantActivity extends AppCompatActivity {
 
     protected ViewGroup btnContainer;
     protected CropImageView cropImageView;
-    protected FloatingActionButton saveSelectedRange, uploadSelectedRange;
-    protected FloatingActionButton shareSelectedRange, editScreenshot, discardScreenshot;
+    protected FloatingActionButton saveCroppedArea, uploadCroppedArea;
+    protected FloatingActionButton shareCroppedArea, editScreenshot, discardScreenshot;
 
 
     @Override
@@ -45,21 +53,55 @@ public class AssistantActivity extends AppCompatActivity {
 
         this.discardScreenshot = this.btnContainer.findViewById(R.id.fabDiscardScreenshot);
         this.editScreenshot = this.btnContainer.findViewById(R.id.fabEditScreenshot);
-        this.saveSelectedRange = this.btnContainer.findViewById(R.id.fabSaveSelectedRange);
-        this.uploadSelectedRange = this.btnContainer.findViewById(R.id.fabUploadSelectedRange);
-        this.shareSelectedRange = this.btnContainer.findViewById(R.id.fabShareSelectedRange);
+        this.saveCroppedArea = this.btnContainer.findViewById(R.id.fabSaveCroppedArea);
+        this.uploadCroppedArea = this.btnContainer.findViewById(R.id.fabUploadCroppedArea);
+        this.shareCroppedArea = this.btnContainer.findViewById(R.id.fabShareCroppedArea);
 
         /* set onclick functions */
         this.btnContainer.setOnClickListener((view) -> this.finish());
         this.editScreenshot.setOnClickListener((view) -> {
+        });
+        this.saveCroppedArea.setOnClickListener((view) -> {
+
+            /* specify target filename using current datetime */
+            @SuppressLint("SimpleDateFormat")
+            File dstFile = new File(
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+                    String.format(
+                            "screenshot-%s.png",
+                            new SimpleDateFormat("yyyyMMddHHmmss").format(Calendar.getInstance().getTime())
+                    )
+            );
+
+            /* save crop area to dst file */
+            String msg = "Save File To " + dstFile.toString();
+            try {
+                saveBitmapTo(this.cropImageView.getCroppedImage(), dstFile);
+
+            } catch (IOException | RuntimeException e) {
+
+                e.printStackTrace();
+                msg += " Failed";
+            }
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
 
         });
-        this.saveSelectedRange.setOnClickListener((view) -> {
+        this.uploadCroppedArea.setOnClickListener((view) -> {
         });
-        this.uploadSelectedRange.setOnClickListener((view) -> {
-        });
-        this.shareSelectedRange.setOnClickListener((view) -> {
+        this.shareCroppedArea.setOnClickListener((view) -> {
 
         });
+    }
+
+
+    protected void saveBitmapTo(Bitmap bitmap, File dst) throws IOException {
+
+        /* specify crop tmp file and create if file not exists */
+        if (!dst.exists() && !dst.createNewFile())
+            throw new RuntimeException(String.format("Create File(%s) Failed.", dst.getAbsoluteFile()));
+
+        /* output cropped area to target file */
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, new FileOutputStream(dst, false));
     }
 }
