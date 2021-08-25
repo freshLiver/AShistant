69c5611d147db5cf85f35e3c93b979cb31b445b6
freshLiver <freshliver.cys@gmail.com>
2021-08-26 01:40:46 +0800

    add set assistant btn in MainActivity & rm testing components in MainActivity

--- app/src/androidTest/java/com/freshliver/ashistant/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/freshliver/ashistant/ExampleInstrumentedTest.kt
@@ -2,11 +2,13 @@ package com.freshliver.ashistant
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.freshliver.ashistant.utils.FileUtils
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import java.io.File
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +22,21 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.freshliver.ashistant", appContext.packageName)
+    }
+
+    @Test
+    fun getFileMimetype() {
+        val input = FileUtils.getMimeTypeFromFile(File("test.png"))
+        val expected = "image/png"
+
+        assertEquals(expected, input)
+    }
+
+    @Test
+    fun getFileExtension() {
+        val input = FileUtils.getExtensionFromFile(File("test.jpeg"))
+        val expected = "jpg"
+
+        assertEquals(expected, input)
     }
 }
--- app/src/main/java/com/freshliver/ashistant/MainActivity.java
+++ app/src/main/java/com/freshliver/ashistant/MainActivity.java
@@ -1,23 +1,24 @@
 package com.freshliver.ashistant;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.provider.Settings;
 import android.widget.Toast;
 
-import com.freshliver.ashistant.main.MainViewModel;
+import com.freshliver.ashistant.assistant.AssistantService;
 import com.google.android.material.button.MaterialButton;
 
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    protected MainViewModel viewModel;
+    protected MaterialButton setAssistant;
 
-    protected TextView showCountView;
-    protected MaterialButton incCount, decCount, loadingBtn;
+    protected ActivityResultLauncher<Intent> setAssistantLauncher;
 
 
     @Override
@@ -26,35 +27,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         /* get ui instances */
-        this.showCountView = this.findViewById(R.id.showCountView);
-        this.incCount = this.findViewById(R.id.incCount);
-        this.decCount = this.findViewById(R.id.decCount);
-        this.loadingBtn = this.findViewById(R.id.loadingBtn);
+        this.setAssistant = this.findViewById(R.id.setAssistantBtn);
 
 
         /* instantiate view model */
-        this.viewModel = new ViewModelProvider(this).get(MainViewModel.class);
 
         /* set observer for view instances */
-        this.viewModel.getCount().observe(this, iValue -> this.showCountView.setText(String.valueOf(iValue)));
-        this.incCount.setOnClickListener(view -> this.viewModel.incCount(true));
-        this.decCount.setOnClickListener(view -> this.viewModel.incCount(false));
-        this.loadingBtn.setOnClickListener((view) -> new LoadingDialog.Builder<String>()
-                .setAsyncTask(() -> this.delay(1000))
-                .setUICallback(result -> Toast.makeText(this, result, Toast.LENGTH_SHORT).show())
-                .build().start(this.getSupportFragmentManager())
+        this.setAssistant.setOnClickListener(view -> this.setDefaultAssistant());
+
+        // register must set before STARTED state
+        this.setAssistantLauncher = this.registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> { }
         );
     }
 
 
-    public String delay(int ms) {
-        try {
-            Thread.sleep(ms);
-            return String.format(Locale.getDefault(), "%d ms!!", ms);
-        }
-        catch (InterruptedException e) {
-            e.printStackTrace();
-            return "Interrupted";
+    private void setDefaultAssistant() {
+
+        // get assistant setting
+        final String assistantSetting = Settings.Secure.getString(this.getContentResolver(), "assistant");
+
+        if (assistantSetting == null)
+            Toast.makeText(this, "Settings.Secure.getString cannot find assistant", Toast.LENGTH_SHORT).show();
+
+        else {
+            // get class name of current digital assistant app and my assistant app
+            final String myAssistant = AssistantService.class.getName();
+            final String nowAssistant = ComponentName.unflattenFromString(assistantSetting).getClassName();
+
+            // if default assistant is not this app, launch assistant setting page
+            if (!myAssistant.equals(nowAssistant))
+                this.setAssistantLauncher.launch(new Intent().setAction(Settings.ACTION_VOICE_INPUT_SETTINGS));
+            else
+                Toast.makeText(this, "Already set as default digital assistant app", Toast.LENGTH_SHORT).show();
         }
     }
 }
